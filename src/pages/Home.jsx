dc75a4c53a31359b3c711949b2e14a2f3c7b7e0c
B2GPT <132028674+Moderatorboy@users.noreply.github.com>
2025-11-24 08:14:52 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -35,7 +35,7 @@ export default function Home() {
 
       {/* Batch Grid */}
       <main className="px-6 pb-12">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           {batches.map((batch, index) => (
             <div
               key={index}
